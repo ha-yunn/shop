d9b66ed5f740ec357256d1883f1aa6be0cc42ff5
ha-yunn <96652845+ha-yunn@users.noreply.github.com>
2022-04-06 01:33:40 +0900

    6장 상품 수정하기

--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -19,6 +21,7 @@ public class ItemImgService {
     private final ItemImgRepository itemImgRepository;
     private final FileService fileService;
 
+    //상품 등록
     public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile)
         throws Exception {
         String oriImgName = itemImgFile.getOriginalFilename();
@@ -35,5 +38,27 @@ public class ItemImgService {
         //상품 이미지 정보 저장
         itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
+    }
+
+    //상품 수정
+
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile)
+        throws Exception {
+        if(!itemImgFile.isEmpty()) {
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId)
+                    .orElseThrow(EntityNotFoundException::new);
+
+            //기존 이미지 파일 삭제
+            if (!StringUtils.isEmpty(savedItemImg.getImgName())) {
+                fileService.deleteFile(itemImgLocation+"/"+
+                                        savedItemImg.getImgName());
+            }
+
+            String oriImgName = itemImgFile.getOriginalFilename();
+            String imgName = fileService.uploadFile(itemImgLocation,
+                    oriImgName, itemImgFile.getBytes());
+            String imgUrl = "/images/item" + imgName;
+            savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        }
     }
 }
