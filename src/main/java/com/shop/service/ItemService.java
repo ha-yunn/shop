ceb89e029feb98af7db0b076b7365f1521d342de
ha-yunn <96652845+ha-yunn@users.noreply.github.com>
2022-04-07 23:47:34 +0900

    6장 상품 관리하기

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -2,11 +2,14 @@ package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
 import com.shop.dto.ItemImgDto;
+import com.shop.dto.ItemSearchDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -65,7 +68,6 @@ public class ItemService {
     }
 
     //상품 수정
-
     public Long updateItem(ItemFormDto itemFormDto,
                            List<MultipartFile> itemImgFileList) throws Exception {
         //상품 수정
@@ -81,5 +83,12 @@ public class ItemService {
                     itemImgFileList.get(i));
         }
         return item.getId();
+    }
+
+    //상품 등록 및 조회
+    @Transactional(readOnly = true)
+    public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto,
+                                       Pageable pageable) {
+        return itemRepository.getAdminItemPage(itemSearchDto, pageable);
     }
 }
